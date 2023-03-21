ccbea529e17e99119262b17e5adab516726cbc1a
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-20 18:32:09 -0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(num){
+    let counter = num;
+    while (counter >= 0) {
+        console.log(counter)
+        counter--
+    }
+}
+
+function writeCards(names, event){
+    const messages = []
+    for (let i = 0 ; i < names.length ; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+    return messages
+}
